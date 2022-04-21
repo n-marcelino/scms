3ff6c045758eaa328e19c22e366eb7d1d9b30719
connorpassingby <connorpassingby@gmail.com>
2022-04-21 21:58:25 +0800

    Update OrderShow.js

--- client/src/OrderShow.js
+++ client/src/OrderShow.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+    useNavigate
+} from "react-router-dom";
+
+import OrderAdd from "./OrderAdd";
 
 export default function OrderShow() {
+
+    const navigate = useNavigate();
 
     //observable variable and function that updates variable
     const [message, setMessage] = useState(""); //string
@@ -47,15 +57,35 @@ export default function OrderShow() {
             )
         } else {
             return (
-                <h2>
-                    No Orders Found
-                </h2>
+                <div>
+                    <h3 className="mb-4 text-danger">
+                        No Orders Found.
+                    </h3>
+
+                    <div className="d-flex flex-wrap gap-3 cc-container">
+                        <div className="bg-light c-card" >
+                            <div onClick={()=>(navigate('/orders/add'))} className="btn h-100 w-100 d-flex align-items-center justify-content-center">
+                                <div>
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        width="82 " 
+                                        height="82" 
+                                        fill="currentColor" 
+                                        className="bi bi-plus-circle-fill text-warning" viewBox="0 0 16 16"
+                                    >
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                                    </svg>
+                                </div>                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )
         }
     }
 
     return ( //returns a single element only; so you can nest all other elements inside one div
-        <div class="p-3">
+        <div className="p-3">
             <h1>
                 Order Record
             </h1>
@@ -64,7 +94,9 @@ export default function OrderShow() {
 
             {renderOrders()}
 
-
+            <Routes>
+                <Route exact path ="/orders/add" element={<OrderAdd/>}/>
+            </Routes>
         </div>
     )
 
