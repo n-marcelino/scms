1a02f91e7cb9c31de64eb1ae6376286a5ee6ca9b
n-marcelino <nicomarcelino0613@gmail.com>
2024-07-04 00:09:55 +0800

    cleaning repo

--- backend/src/main/java/com/ciit/scms/models/Category.java
+++ backend/src/main/java/com/ciit/scms/models/Category.java
@@ -1,55 +1,8 @@
 package com.ciit.scms.models;
 
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="categories")
 public class Category {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(name = "name", nullable = false)
-	private String name;
-	
-	@OneToMany(fetch=FetchType.EAGER, mappedBy="category")//eager vs lazy; eager pulls out category from database and queries at the same  time
-	Set<Product> products;
-	//not a column, but a query mapping
-	//a query to fetch all products of a single category
-	
-	//SETTERS AND GETTERS
-	
-	public Integer getId() {
-		return id;
-	}
+    private int id;
+    private String name;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Set<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(Set<Product> products) {
-		this.products = products;
-	}	
-	
 }
